bf6c4f0a0321af451767b3b4fe8fa58c8be8837b
vedabrat <vedabrat>
2017-10-27 17:50:16 -0400

    chap7assn done

--- models/address_book.rb
+++ models/address_book.rb
@@ -61,4 +61,14 @@ class AddressBook
     # #5
     return nil
   end
+
+  def iterative_search(name)
+    entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
+    return nil
+
+  end
 end
